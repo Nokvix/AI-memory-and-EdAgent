1304d31a5e8d65a0d8ca9bdce98c29d01bdf363a
jnxcomit <rashad100aliev@gmail.com>
2025-12-18 19:42:44 +0500

    поменял скоринг

--- app/services/scoring.py
+++ app/services/scoring.py
@@ -1,3 +1,3 @@
 def calculate_score(vacancy_count: int, skills_found: int) -> float:
-    raw_score = (vacancy_count * 10 + skills_found * 20)
-    return min(float(raw_score), 100.0)
+    raw_score = (vacancy_count * 5) + (skills_found * 10)
+    return float(raw_score)
--- check_db.py
+++
@@ -1,6 +0,0 @@
-from app.core.database import engine, Base
-from app.models.models import Company
-
-print("Создаем таблицы...")
-Base.metadata.create_all(bind=engine)
-print("Успех! Файл mvp.db должен появиться.")
