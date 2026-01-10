d038d7d04cab86cd7c1666e7704d1d1e0e119458
jnxcomit <rashad100aliev@gmail.com>
2026-01-10 12:57:52 +0500

    финальные правки для докера

--- backend/app/core/database.py
+++ backend/app/core/database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./mvp_database.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:////data/mvp_database.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
